5378b3c9edac0c0631e2f6db7c9e15e24d2d31c8
Osmandi <osmandigt@gmail.com>
2024-10-18 14:42:00 -0500

    Documentación de funciones de EDA.py

--- scripts/EDA.py
+++ scripts/EDA.py
@@ -6,12 +6,33 @@ from seaborn import heatmap
 from typing import List, Optional
 
 def strip_columns(df: pd.DataFrame, columns: List[str]) -> pd.DataFrame:
-    # Apply strip
+    """
+    Elimina espacios en blanco al inicio y al final de los valores de las columnas especificadas en un DataFrame.
+    
+    Parámetros:
+    - df (pd.DataFrame): El DataFrame sobre el cual se aplicará la limpieza de columnas.
+    - columns (List[str]): Lista de nombres de las columnas a las que se les debe aplicar el método `strip`.
+    
+    Retorno:
+    - pd.DataFrame: El DataFrame original con los espacios en blanco eliminados de las columnas especificadas.
+    """
     for col in columns:
         df[col] = df[col].str.strip()
     return df
 
 def create_corr_graph(df: pd.DataFrame, title: str, path: str):
+    """
+    Genera un gráfico de calor de la matriz de correlación para las variables numéricas del DataFrame
+    y lo guarda en un archivo PNG.
+    
+    Parámetros:
+    - df (pd.DataFrame): El DataFrame del cual se extraerá la matriz de correlación.
+    - title (str): Título que se usará para el gráfico y el nombre del archivo guardado.
+    - path (str): Ruta donde se guardará la imagen del gráfico.
+    
+    Retorno:
+    - None: Esta función no retorna ningún valor, pero guarda el gráfico de correlación en la ubicación especificada.
+    """
 
     # Create correlation matrix for numeric only
     ## and filter only for the higest correlation values
@@ -30,14 +51,50 @@ def create_corr_graph(df: pd.DataFrame, title: str, path: str):
     plt.savefig(f"{path}/reports/{title}_correlation.png")
 
 def generate_sweetviz_report(df: pd.DataFrame, title: str, path: str):
+    """
+    Genera un informe de análisis exploratorio de datos (EDA) utilizando la biblioteca Sweetviz 
+    y lo guarda en formato HTML.
+    
+    Parámetros:
+    - df (pd.DataFrame): El DataFrame que se analizará.
+    - title (str): Título que se usará para el informe generado.
+    - path (str): Ruta donde se guardará el informe en HTML.
+    
+    Retorno:
+    - None: Esta función no retorna ningún valor, pero guarda el informe en la ubicación especificada.
+    """
     df_report = sv.analyze(df)
     df_report.show_html(f"{path}/reports/{title}_EDA.html")
 
 def generate_parquet(df: pd.DataFrame, title: str, path: str):
+    """
+    Guarda un DataFrame en un archivo en formato Parquet, que es un formato de almacenamiento columnar eficiente.
+    
+    Parámetros:
+    - df (pd.DataFrame): El DataFrame que se quiere guardar.
+    - title (str): Nombre del archivo que se generará.
+    - path (str): Ruta donde se guardará el archivo Parquet.
+    
+    Retorno:
+    - None: Esta función no retorna ningún valor, pero guarda el DataFrame en un archivo Parquet en la ubicación especificada.
+    """
     df.to_parquet(f"{path}/dataframes/{title}.parquet", index=False)
 
 def generate_eda(df: pd.DataFrame, title: str, path: str, exclude_colums_eda: Optional[List[str]] = None):
-
+    """
+    Genera un análisis exploratorio de datos (EDA) guardando un archivo Parquet, creando un gráfico de
+    correlación y generando un informe de Sweetviz. Permite excluir columnas específicas del análisis.
+    
+    Parámetros:
+    - df (pd.DataFrame): El DataFrame que se analizará.
+    - title (str): Título que se usará para los archivos generados.
+    - path (str): Ruta donde se guardarán los archivos generados.
+    - exclude_colums_eda (Optional[List[str]]): Lista de nombres de columnas que se deben excluir del análisis. 
+      Por defecto es `None`.
+    
+    Retorno:
+    - None: Esta función no retorna ningún valor, pero realiza varias acciones que incluyen guardar archivos y generar informes.
+    """
     # Export csv cleaned
     generate_parquet(df, title, path)
 
